adf3d16c408124e619202758baa7b5ae4c957acf
Guilherme Pereira <guilherme.pereira17@outlook.com>
2018-05-09 14:48:36 -0300

    created project

---
+++ src/pages/home/home.ts
@@ -0,0 +1,41 @@
+import { Component , OnInit , ViewChild, Input, EventEmitter, Output} from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { UserProvider } from '../../providers/user/user';
+import { Observable } from 'rxjs/Observable';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(
+    public navCtrl: NavController,
+    private usersService: UserProvider
+  ) {  }
+
+  public users: any = [];
+
+  ngOnInit() {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    
+    //let usersObservable: Observable<[any]>;
+    
+        let usersObservable = this.usersService.getUser();
+    
+        usersObservable.subscribe(
+          data => {
+            this.users = data;
+            console.log(this.users);
+          },
+          err => {
+            
+            this.users = [];
+          }
+        )
+      }
+}
---
+++ src/providers/user/user.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the UserProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class UserProvider {
+
+  constructor(public http: HttpClient) {
+  }
+
+  getUser(){
+     return this.http.get('http://localhost:54967/api/users');
+  }
+}
